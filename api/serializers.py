eb670432e583b6906c81a25e8a8533f4bd69dc90
Jimmy_Tempest <smartwattana@gmail.com>
2021-07-04 00:48:46 +0700

    remove libraryof from serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,15 +1,16 @@
 from rest_framework import serializers
 from .models import *
 
+
 class DatasetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dataset
-        fields = '__all__'
-        depth = 1    
+        exclude = ['libraryOf']
+        depth = 1
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = ['password']
-        depth = 1    
-    
+        depth = 1
