c22d8dcf962ef668a4ac039909fa5af4b1c33f41
Jimmy_Tempest <smartwattana@gmail.com>
2021-07-05 23:04:15 +0700

    signin signup done

--- static/frontend/script.js
+++ static/frontend/script.js
@@ -1,7 +1,34 @@
 const Link = ReactRouterDOM.Link;
 const Route = ReactRouterDOM.Route;
+const Redirect = ReactRouterDOM.Redirect;
 
-
+function getCookies() {
+    var c = document.cookie, v = 0, cookies = {};
+    if (document.cookie.match(/^\s*\$Version=(?:"1"|1);\s*(.*)/)) {
+        c = RegExp.$1;
+        v = 1;
+    }
+    if (v === 0) {
+        c.split(/[,;]/).map(function(cookie) {
+            var parts = cookie.split(/=/, 2),
+                name = decodeURIComponent(parts[0].trimLeft()),
+                value = parts.length > 1 ? decodeURIComponent(parts[1].trimRight()) : null;
+            cookies[name] = value;
+        });
+    } else {
+        c.match(/(?:^|\s+)([!#$%&'*+\-.0-9A-Z^`a-z|~]+)=([!#$%&'*+\-.0-9A-Z^`a-z|~]*|"(?:[\x20-\x7E\x80\xFF]|\\[\x00-\x7F])*")(?=\s*[,;]|$)/g).map(function($0, $1) {
+            var name = $0,
+                value = $1.charAt(0) === '"'
+                          ? $1.substr(1, -1).replace(/\\(.)/g, "$1")
+                          : $1;
+            cookies[name] = value;
+        });
+    }
+    return cookies;
+}
+function getCookie(name) {
+    return getCookies()[name];
+}
 
 class App extends React.Component{
     render(){
@@ -90,7 +117,7 @@ class Base extends React.Component {
 class Home extends Base {
     render() {
         return (
-        <div>
+        <div className="div-container">
             {this.renderMenu()}
             <div className="content-container">Hello World</div>
         </div>
@@ -101,15 +128,73 @@ class Home extends Base {
 class Signin extends Base {
     constructor(props){
         super(props)
-        this.state = { ...this.state, signup:true}
+        this.state = {
+             ...this.state, 
+             signup:true,
+             redirect:false,
+             username:"",
+             password:"",
+             conf_password:"",
+             email:"",
+             passAlert:false
+            }
         this.changeState = this.changeState.bind(this)
+        this.SetSignin = this.SetSignin.bind(this)
+        this.SetSignup = this.SetSignup.bind(this)
+        this.updateUsername = this.updateUsername.bind(this)
+        this.updatePassword = this.updatePassword.bind(this)
+        this.updateEmail = this.updateEmail.bind(this)
+        this.updateConPass = this.updateConPass.bind(this)
+        this.signIn = this.signIn.bind(this)
+        this.signUp = this.signUp.bind(this)
+        this.checkAlert = this.checkAlert.bind(this)
+        
     }
 
     renderForm(){
+        if (this.state.redirect)
+        return <Redirect to="/" />
         if (this.state.signup)
-        return <span>Sign up</span>
+        return (
+            <div>
+                <div className="selector">
+                <div id="signup" onClick={this.SetSignup}>Sign in</div>
+                    <div id="signin" onClick={this.SetSignin} >Sign up</div>
+                    <hr className="slider" />
+                </div>
+                <form onSubmit={this.signIn}>
+                    <input type="text" placeholder="Username" className="textInput" id="username" autofocus="true" value={this.state.username} onChange={this.updateUsername}></input>
+                    <input type="password" placeholder="Password" className="textInput" id="password" value={this.state.password} onChange={this.updatePassword}></input>
+                    <input type="submit" className="submitButton" value="Sign in"></input>
+                    <div className="submitButton" onClick={this.changeState} >Sign Up?</div>
+                </form>
+            </div>
+            )
         else
-        return <span>Sign in</span>
+        return (
+            <div>
+                <div className="selector">
+                    <div id="signup" onClick={this.SetSignup}>Sign in</div>
+                    <div id="signin" onClick={this.SetSignin} className="active">Sign up</div>
+                    <hr className="slider" />
+                </div>
+                <form onSubmit={this.signUp}>
+                    <input type="text" placeholder="Username" className="textInput" id="username" autofocus="true" value={this.state.username} onChange={this.updateUsername}></input>
+                    <input type="email" placeholder="Email" className="textInput" id="email" value={this.state.email} onChange={this.updateEmail}></input>
+                    <input type="password" placeholder="Password" className="textInput" id="password" value={this.state.password} onChange={this.updatePassword}></input>
+                    <input type="password" placeholder="Confirm Password" className="textInput" id="password2" value={this.state.conf_password} onChange={this.updateConPass}></input>
+                    {this.checkAlert()}
+                    <input type="submit" className="submitButton" value="Sign up"></input>
+                    <div className="submitButton" onClick={this.changeState} >Sign In?</div>
+                </form>
+            </div>
+            )
+    }
+    checkAlert(){
+        if(this.state.passAlert){
+        
+        return <span className="error">The passwords you entered do not match</span>
+        }
     }
 
     changeState() {
@@ -119,14 +204,110 @@ class Signin extends Base {
             })
         )
     }
+    SetSignin() {
+        this.setState(prevState => ({
+                ...this.state,
+                signup:false
+            })
+        )
+    }
+    SetSignup() {
+        this.setState(prevState => ({
+                ...this.state,
+                signup:true
+            })
+        )
+    }
+
+    updateUsername(event){
+        this.setState(prevState => ({
+            ...this.state,
+            username:event.target.value
+        })
+    )
+    }
+    updatePassword(event){
+        this.setState(prevState => ({
+            ...this.state,
+            password:event.target.value
+        })
+    )
+    }
+    updateEmail(event){
+        this.setState(prevState => ({
+            ...this.state,
+            email:event.target.value
+        })
+    )
+    }
+    updateConPass(event){
+        this.setState(prevState => ({
+            ...this.state,
+            conf_password:event.target.value
+        })
+    )
+    }
+
+    signIn(){
+        fetch('/api/login', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json; charset=UTF-8", 'X-CSRFToken': getCookie('csrftoken') },
+        body: JSON.stringify({
+            "username": this.state.username,
+            "password": this.state.password
+        })
+        })
+        .then(response => response.json())
+        .then(result => {
+            // Print result
+            this.setState(prevState => ({
+                ...this.state,
+                redirect:true
+            })
+            )
+            return false
+        });
+    }
+    signUp(){
+        if (this.state.password === this.state.conf_password){
+        fetch('/api/register', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json; charset=UTF-8", 'X-CSRFToken': getCookie('csrftoken') },
+        body: JSON.stringify({
+            "username": this.state.username,
+            "password": this.state.password,
+            "email": this.state.email
+        })
+        })
+        .then(response => response.json())
+        .then(result => {
+            // Print result
+            this.setState(prevState => ({
+                ...this.state,
+                redirect:true
+            })
+            )
+            return false
+        });
+    }
+    else
+    {
+        this.setState(prevState => ({
+            ...this.state,
+            passAlert:true
+        })
+        )
+    }
+    }
 
     render() {
         return (
-        <div>
+        <div className="div-container">
             {this.renderMenu()}
             <div className="content-container">
-            {this.renderForm()}
-            <button onClick={this.changeState}>Click</button>
+                <div className="login-form-container">
+                {this.renderForm()}
+                </div>
             </div>
         </div>
         )
