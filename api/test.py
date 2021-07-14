922de2e5640d7e4fa17ffc7a83f7c583278a4e3c
Jimmy_Tempest <smartwattana@gmail.com>
2021-07-14 21:27:24 +0700

    remove test.py

--- api/test.py
+++
@@ -1,9 +0,0 @@
-import tensorflowjs as tfjs
-from tensorflow import keras
-
-
-test = tfjs.converters.load_keras_model(
-    "./media_root/models/user_1/14/model.json")
-print(test.summary())
-
-tfjs.converters.save_keras_model(test, "./media_root/models/user_1/13")
